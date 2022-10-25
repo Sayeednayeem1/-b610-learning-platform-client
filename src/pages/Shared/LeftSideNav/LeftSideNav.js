27eaf979f3c3c1b8166901029ce1921db725402a
Abu Sayeed <sayeed@pop-os.localdomain>
2022-10-25 14:12:47 +0600

    updated responsive haeder

--- src/pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/language-categories')
+        .then(res => res.json())
+        .then(data => setCategories(data));
+    }, [])
+
     return (
         <div>
-            <h5>This is LeftSideNav</h5>
+            <h4>All categories: {categories.length}</h4>
+            <div>
+                {
+                    categories.map( c => <p key={c.id}>
+                        <Link to={`/category/${c.id}`}>{c.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
