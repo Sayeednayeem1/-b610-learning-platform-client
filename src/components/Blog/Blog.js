ba2d32511fc42d1bd68655ac9dd7f1d5d4fbc17f
Abu Sayeed <sayeed@pop-os.localdomain>
2022-10-26 23:56:30 +0600

    updated the blog and FAQ routes background color

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,26 +1,37 @@
 import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const Blog = () => {
     return (
-        <div>
-            <div className='text-secondary text-center mt-5 mb-5'>
-                <h2>This is blog section</h2>
-                <h4>All the question answers are provided below</h4>
+        <div className='blog'>
+            <div className='blog-padding'>
+                <div className='text-secondary text-center'>
+                    <h2>This is blog section</h2>
+                    <h4>All the question answers are provided below</h4>
+                </div>
+                <div className='ms-5 '>
+                    <h3>Q-1: What is cors?</h3>
+                    <p className='ms-4'>Ans: CORS stands for Cross-Origin Resource Sharing . It allows us to relax the security applied to an API. This is done by bypassing the Access-Control-Allow-Origin headers, which specify which origins can access the API.</p>
+                    <br />
+                    <h3>Q-2: Why are you using firebase? What other options do you have to implement authentication?</h3>
+                    <p className='ms-4'>Ans: Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.</p>
+                    <br />
+                    <h3>Q-3:  How does the private route work?</h3>
+                    <p className='ms-4'>Ans: The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in)</p>
+                    <br />
+                    <h3>Q-4: What is Node? How does Node work?</h3>
+                    <p className='ms-4'>Ans: 1. Node allows developers to write JavaScript code that runs directly in a computer process itself instead of in a browser.</p>
+                    <p className='ms-4'>Node allows developers to write JavaScript code that runs directly in a computer process itself instead of in a browser. Node can, therefore, be used to write server-side applications with access to the operating system, file system, and everything else required to build fully-functional applications.</p>
+                </div>
             </div>
-            <div className='ms-5 '>
-                <h3>Q-1: What is cors?</h3>
-                <p className='ms-4'>Ans: CORS stands for Cross-Origin Resource Sharing . It allows us to relax the security applied to an API. This is done by bypassing the Access-Control-Allow-Origin headers, which specify which origins can access the API.</p>
-                <br/>
-                <h3>Q-2: Why are you using firebase? What other options do you have to implement authentication?</h3>
-                <p className='ms-4'>Ans: Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.</p>
-                <br/>
-                <h3>Q-3:  How does the private route work?</h3>
-                <p className='ms-4'>Ans: The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in)</p>
-                <br/>
-                <h3>Q-4: What is Node? How does Node work?</h3>
-                <p className='ms-4'>Ans: 1. Node allows developers to write JavaScript code that runs directly in a computer process itself instead of in a browser.</p>
-                <p className='ms-4'>Node allows developers to write JavaScript code that runs directly in a computer process itself instead of in a browser. Node can, therefore, be used to write server-side applications with access to the operating system, file system, and everything else required to build fully-functional applications.</p>
-            </div>
+            <footer className='text-center bg-info p-4'>
+                <p><small>Alright reserved @2023</small></p>
+                <div>
+                    <FaFacebook className='fs-1'></FaFacebook>
+                    <FaTwitter className='ms-5 fs-1'></FaTwitter>
+                    <FaInstagram className='ms-5 fs-1'></FaInstagram>
+                </div>
+            </footer>
         </div>
     );
 };
--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,79 +1,78 @@
 import React from 'react';
 import Accordion from 'react-bootstrap/Accordion';
-
-
 import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const Faq = () => {
     return (
-        <div>
+        <div className='faq'>
             <div>
-                <div className='text-center mx-auto'>
+                <div className='text-center mx-auto p-5'>
                     <h2>Tools Of Web Development</h2>
                     <p>In this route i will talk about the important tools of web development </p>
                 </div>
-                <Accordion defaultActiveKey={['0']} alwaysOpen>
-                    <Accordion.Item eventKey="1">
-                        <Accordion.Header>HTML</Accordion.Header>
-                        <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="2">
-                        <Accordion.Header>CSS</Accordion.Header>
-                        <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="3">
-                        <Accordion.Header>Javascript</Accordion.Header>
-                        <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="4">
-                        <Accordion.Header>React</Accordion.Header>
-                        <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="5">
-                        <Accordion.Header>Node js</Accordion.Header>
-                        <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                </Accordion>
+                <div className='accordion'>
+                    <Accordion  defaultActiveKey={['0']} alwaysOpen>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>HTML</Accordion.Header>
+                            <Accordion.Body>
+                                <p>
+                                    Hypertext Markup Language, or HTML, is a programming language used to describe the structure of information on a webpage. Together, HTML, CSS, and JavaScript make up the essential building blocks of websites worldwide, with CSS controlling a page's appearance and JavaScript programming its functionality.</p>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header>CSS</Accordion.Header>
+                            <Accordion.Body>
+                                <p>
+                                    CSS makes the front-end of a website shine and it creates a great user experience. Without CSS, websites would be less pleasing to the eye and likely much harder to navigate. In addition to layout and format, CSS is responsible for font color and more.
+                                </p>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="3">
+                            <Accordion.Header>Javascript</Accordion.Header>
+                            <Accordion.Body>
+                                <p>
+                                    JavaScript can make the website more interactive and user- friendliness of JavaScript helps easy navigation of the website and helps designers to guide the visitors with additional information or guide them through walkthroughs. Visual effects can also be achieved with JavaScript.
+                                </p>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="4">
+                            <Accordion.Header>React</Accordion.Header>
+                            <Accordion.Body>
+                                <p>
+                                    React is an excellent tool with which to create interactive applications for mobile, web, and other platforms. React's popularity and usage are increasing day by day for good reason. As a developer, coding in React makes you better at JavaScript, a language that holds nearly 90% of the web development share today.
+                                </p>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="5">
+                            <Accordion.Header>Node js</Accordion.Header>
+                            <Accordion.Body>
+                                <p>
+                                    Node. js is a platform built on Chrome's JavaScript runtime for easily building fast and scalable network applications. Node. js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices
+                                </p>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                </div>
+                <div className='mt-5 self-adv'>
+                    <h4>However this are the steps you can follow!</h4>
+                    <br/>
+                    <p>1. Understand the key concepts of visual design</p>
+                    <br/>
+                    <p>2. Know the basics of HTML</p>
+                    <br/>
+                    <p>3. Understand CSS</p>
+                    <br/>
+                    <p>4. Learn basic javascript seriously</p>
+                    <br/>
+                    <p>5. Learn React. Its cool!</p>
+                    <br/>
+                    <p>6. Understand the basics of creating layouts</p>
+                    <br/>
+                    <p>7. Put your knowledge into action and build something</p>
+                    <br/>
+                    <p>8. Take a break once in awhile</p>
+                    <br/>
+                </div>
             </div>
             <footer className='text-center bg-info p-4'>
                 <p><small>Alright reserved @2023</small></p>
